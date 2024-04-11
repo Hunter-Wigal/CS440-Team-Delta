c4a4f99901f085fcdd3522a3774ea18aecc35afd
Brandon Hines <bah00025@mix.wvu.edu>
2024-04-11 14:25:41 -0400

    Added alter user root statement

--- delta_marketplace/create_db.py
+++ delta_marketplace/create_db.py
@@ -9,7 +9,10 @@ database = mysql.connector.connect(
 # cursor
 cursor = database.cursor()
 
+# If you get a password error, uncomment and run the line below
+# cursor.execute("ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';")
+
 # Create database
-cursor.execute("CREATE DATABASE delta_marketplace")
+cursor.execute("CREATE DATABASE IF NOT EXISTS delta_marketplace")
 
 print("Created database")

df6a48a51dd94dff3dd35ca3666bebeb32a1dc50
Brandon Hines <bah00025@mix.wvu.edu>
2024-04-22 05:54:18 -0400

    run refresh_db.py when fetching from main, mega time saver

--- delta_marketplace/create_db.py
+++ delta_marketplace/create_db.py
@@ -14,3 +14,81 @@ cursor = database.cursor()
 
 # Create database
 cursor.execute("CREATE DATABASE IF NOT EXISTS delta_marketplace")
+
+cursor.execute("""USE delta_marketplace;""")               
+               
+cursor.execute("""CREATE TABLE IF NOT EXISTS Moderators (
+    mod_id INT PRIMARY KEY
+);""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Publishers(
+	publisher_id INT PRIMARY KEY,
+    mod_id INT,
+    publisher_name VARCHAR(30) UNIQUE NOT NULL,
+    location VARCHAR(30),
+    FOREIGN KEY (mod_id) REFERENCES Moderators(mod_id)
+		ON UPDATE CASCADE
+		ON DELETE RESTRICT
+);""")
+
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Games(
+	game_id INT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    esrb VARCHAR(10) DEFAULT 'PENDING',
+    release_date DATE NOT NULL,
+    genre VARCHAR(15),
+    publisher_id INT NOT NULL,
+    FOREIGN KEY (publisher_id) REFERENCES Publishers(publisher_id)
+		ON UPDATE CASCADE
+        ON DELETE RESTRICT
+        
+);""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Users(
+	username VARCHAR(30) PRIMARY KEY,
+    display_name VARCHAR(30),
+    full_name VARCHAR(50) NOT NULL,
+    birth_date DATE NOT NULL
+);""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Collectibles(
+	collectible_id INT PRIMARY KEY,
+    game_id INT NOT NULL,
+    image BLOB,
+    collectible_name VARCHAR(30),
+    UNIQUE(collectible_id, game_id),
+    FOREIGN KEY (game_id) REFERENCES Games(game_id)
+		ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS GamesOwned (
+    username VARCHAR(30),
+    game_id INT PRIMARY KEY,
+    owned_start DATE NOT NULL,
+    owned_end DATE,
+    FOREIGN KEY (username) REFERENCES Users (username)
+		ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    FOREIGN KEY (game_id) REFERENCES Games (game_id)
+		ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS CollectiblesOwned(
+	username VARCHAR(30) ,
+    collectible_id INT NOT NULL,
+	game_id INT NOT NULL,
+    UNIQUE(collectible_id, game_id),
+	PRIMARY KEY(username, collectible_id),
+    FOREIGN KEY (username) REFERENCES Users(username)
+		ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    FOREIGN KEY (collectible_id) REFERENCES Collectibles(collectible_id)
+		ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    FOREIGN KEY (game_id) REFERENCES Games(game_id)
+		ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);""")
--- delta_marketplace/users_api/urls.py
+++ delta_marketplace/users_api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get_user', views.get_user)
+    path('get_user', views.get_user),
+    path('get_ownedGames', views.get_owned_games),
+    path('get_ownedCollectibles', views.get_owned_coll)
 ]
